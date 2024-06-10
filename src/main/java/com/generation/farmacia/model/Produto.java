c42030e43ff05244a73e5a711a7fd1c3505a957d
Your Name <you@example.com>
2024-06-10 10:38:01 -0300

    crud completo da categoria

--- src/main/java/com/generation/farmacia/model/Produto.java
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -1,10 +1,75 @@
 package com.generation.farmacia.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
 @Entity
 @Table(name="tb_produtos")
 public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) 
+	private Long id;
+	
+	@NotBlank(message = "O atributo NOME é Obrigatório!")
+	private String nome;
+	
+	@NotBlank(message = "O atributo DESCRIÇÃO é Obrigatório!")
+	@Size (min = 10, max = 2500, message= "O atributo texto deve conter no minimo 10 e no máximo 2500 caracteres")
+	private String descricao;
+	
+	@NotNull(message = "O atributo PREÇO é Obrigatório!")
+	private float preco;
+	
+	@NotNull(message = "O atributo QUANTIDADE é Obrigatório!")
+	private int quantidade;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public float getPreco() {
+		return preco;
+	}
+
+	public void setPreco(float preco) {
+		this.preco = preco;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	
 
 }
